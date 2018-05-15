bf4249f1cb28fe6aceca6586689fff8657629e66
ondretom <tondrej@seznam.cz>
2018-05-15 12:36:30 +0200

    Fix travis assets

--- scripts/artifactory-deploy.sh
+++ scripts/artifactory-deploy.sh
@@ -1,12 +1,13 @@
 #!/bin/sh
 
 PATH_TO_FILE=$1
-TARGET_FILE_PATH=$2
-
-if [ -z "$2" ]; then
-    TARGET_FILE_PATH=$1
-fi
 
 DIR_URL=https://na.artifactory.swg-devops.com/artifactory/iot-waw-trevis-generic-local/${TRAVIS_BRANCH}/${TRAVIS_BUILD_NUMBER}
 
-curl -H 'X-JFrog-Art-Api: '${API_KEY} -T ${PATH_TO_FILE} ${DIR_URL}/${TARGET_FILE_PATH}
+# TODO It does not work for with nested folders
+echo "Artifactory: deploy ${PATH_TO_FILE}";
+for filename in ${PATH_TO_FILE}; do
+    echo "Artifactory: deploy ${filename} to ${DIR_URL}/${filename}";
+    curl -H 'X-JFrog-Art-Api: '${API_KEY} -T ${filename} ${DIR_URL}/${filename};
+    echo "\n";
+done
--- scripts/travis.sh
+++ scripts/travis.sh
@@ -1,86 +1,125 @@
 #!/bin/sh
 
-stopIfFailed() {
-    if (($? != 0)); then
-        echo "Previous command failed, stop the build"
-        exit(2)
+export API_KEY=AKCp5aU5jWcMLvD2ViDN3jV2dtitVbrg2cq1aftQTUXCyGdAc3DjWXBemYFsuduFsAhnpcJ8o
+export WA_PASSWORD=ptFbCMZ2hCC1
+export WA_USERNAME=02045182-1793-440c-b28c-7da07dca3117
+export WA_WORKSPACE_ID_MASTER=4a64f694-0608-4e38-8ccc-eba2dcd30c7e
+export WA_WORKSPACE_ID_TEST=f2e3f277-61a3-40d2-92f9-c3c77f34fb31
+export TRAVIS_BRANCH=local
+export TRAVIS_BUILD_NUMBER=2
+
+stopIfFailed() 
+{
+    if [ "$1" -ne "0" ]; then
+        echo "--------------------------------------------------------------------------------";
+echo "-- Previous command failed, stop the build!";
+echo "--------------------------------------------------------------------------------";
+        exit 2
     fi
 }
 
-echo "Dialog, intents from XLS to XML, CSV"
-mkdir -p tests/data/dialog/generated
-python scripts/dialog_xls2xml.py -x tests/data/dialog/xls/WEATHER-FAQ-CZ.xlsx -gd tests/data/dialog/generated -gi "tests/data/intents" -v
-./scripts/artifactory-deploy.sh tests/data/dialog/generated/*
-stopIfFailed()
+echo "--------------------------------------------------------------------------------";
+echo "-- Dialog, intents from XLS to XML, CSV";
+echo "--------------------------------------------------------------------------------";
+mkdir -p tests/data/dialog/generated;
+python scripts/dialog_xls2xml.py -x tests/data/dialog/xls/WEATHER-FAQ-CZ.xlsx -gd tests/data/dialog/generated -gi "tests/data/intents" -v;
+stopIfFailed $?;
+./scripts/artifactory-deploy.sh "tests/data/dialog/generated/*";
 
-echo "Dialog from XML to JSON"
-mkdir -p outputs
-python scripts/dialog_xml2json.py -dm tests/data/dialog/main.xml -of outputs -od dialog.json -s ../data_spec/dialog_schema.xml -v
-./scripts/artifactory-deploy.sh outputs/dialog.json
-stopIfFailed()
+echo "--------------------------------------------------------------------------------";
+echo "-- Dialog from XML to JSON";
+echo "--------------------------------------------------------------------------------";
+mkdir -p outputs;
+python scripts/dialog_xml2json.py -dm tests/data/dialog/main.xml -of outputs -od dialog.json -s ../data_spec/dialog_schema.xml -v;
+stopIfFailed $?;
+./scripts/artifactory-deploy.sh outputs/dialog.json;
 
-echo "Entities from CSV to JSON"
+echo "--------------------------------------------------------------------------------";
+echo "-- Entities from CSV to JSON";
+echo "--------------------------------------------------------------------------------";
 mkdir -p outputs
 python scripts/entities_csv2json.py -ie tests/data/entities/ -oe entities.json -od outputs -v
+stopIfFailed $?;
 ./scripts/artifactory-deploy.sh outputs/entities.json
-stopIfFailed()
 
-echo "Intents from CSV to JSON"
+echo "--------------------------------------------------------------------------------";
+echo "-- Intents from CSV to JSON";
+echo "--------------------------------------------------------------------------------";
 mkdir -p outputs
 python scripts/intents_csv2json.py -ii tests/data/intents/ -od outputs -oi intents.json -v
+stopIfFailed $?;
 ./scripts/artifactory-deploy.sh outputs/intents.json
-stopIfFailed()
 
-echo "Compose workspace"
+echo "--------------------------------------------------------------------------------";
+echo "-- Compose workspace";
+echo "--------------------------------------------------------------------------------";
 python scripts/workspace_compose.py -of outputs -ow workspace.json -oe entities.json -od dialog.json -oi intents.json -v
+stopIfFailed $?;
 ./scripts/artifactory-deploy.sh outputs/workspace.json
-stopIfFailed()
 
-echo "Decompose workspace"
+echo "--------------------------------------------------------------------------------";
+echo "-- Decompose workspace";
+echo "--------------------------------------------------------------------------------";
 python scripts/workspace_decompose.py outputs/workspace.json -i outputs/intentsOut.json -e outputs/entitiesOut.json -d outputs/dialogOut.json -v
+stopIfFailed $?;
 ./scripts/artifactory-deploy.sh outputs/intentsOut.json
 ./scripts/artifactory-deploy.sh outputs/entitiesOut.json
 ./scripts/artifactory-deploy.sh outputs/dialogOut.json
-stopIfFailed()
 
-echo "Intents from JSON to CSV"
+echo "--------------------------------------------------------------------------------";
+echo "-- Intents from JSON to CSV";
+echo "--------------------------------------------------------------------------------";
 mkdir -p outputs/intents
 python scripts/intents_json2csv.py outputs/intentsOut.json outputs/intents/ -v
-./scripts/artifactory-deploy.sh outputs/intents/*
-stopIfFailed()
+stopIfFailed $?;
+./scripts/artifactory-deploy.sh "outputs/intents/*";
 
-echo "Entities from JSON to CSV"
+echo "--------------------------------------------------------------------------------";
+echo "-- Entities from JSON to CSV";
+echo "--------------------------------------------------------------------------------";
 mkdir -p outputs/entities
 python scripts/entities_json2csv.py outputs/entitiesOut.json outputs/entities -v
-./scripts/artifactory-deploy.sh outputs/entities/*
-stopIfFailed()
+stopIfFailed $?;
+./scripts/artifactory-deploy.sh "outputs/entities/*";
 
-echo "Dialog from JSON to XML"
+echo "--------------------------------------------------------------------------------";
+echo "-- Dialog from JSON to XML";
+echo "--------------------------------------------------------------------------------";
 mkdir -p outputs/dialog
 python scripts/dialog_json2xml.py outputs/dialogOut.json -d outputs/dialog/ -v
-./scripts/artifactory-deploy.sh outputs/dialog/*
-stopIfFailed()
+stopIfFailed $?;
+./scripts/artifactory-deploy.sh "outputs/dialog/*";
 
-echo "Deploy test workspace"
+echo "--------------------------------------------------------------------------------";
+echo "-- Deploy test workspace";
+echo "--------------------------------------------------------------------------------";
 python scripts/workspace_deploy.py -of outputs -ow workspace.json -c tests/test.cfg -v -cn $WA_USERNAME -cp $WA_PASSWORD -cid $WA_WORKSPACE_ID_TEST
-stopIfFailed()
+stopIfFailed $?;
 
-echo "Test workspace"
+echo "--------------------------------------------------------------------------------";
+echo "-- Test workspace";
+echo "--------------------------------------------------------------------------------";
 # TODO Get rid of this when workspace_test.py allows cmd params
-echo "username = ${WA_USERNAME}" >> tests/test.cfg
-echo "password = ${WA_PASSWORD}" >> tests/test.cfg
-echo "workspace_id = ${WA_WORKSPACE_ID_TEST}" >> tests/test.cfg
+cp tests/test.cfg tests/tmp.cfg;
+echo "username = ${WA_USERNAME}" >> tests/tmp.cfg;
+echo "password = ${WA_PASSWORD}" >> tests/tmp.cfg;
+echo "workspace_id = ${WA_WORKSPACE_ID_TEST}" >> tests/tmp.cfg;
 mkdir -p outputs/dialog
-python scripts/workspace_test.py tests/test.cfg tests/test_more_outputs.test outputs/test_more_outputs.out -v
+python scripts/workspace_test.py tests/tmp.cfg tests/test_more_outputs.test outputs/test_more_outputs.out -v
+stopIfFailed $?;
 ./scripts/artifactory-deploy.sh outputs/test_more_outputs.out
-stopIfFailed()
+rm -f tests/tmp.cfg;
 
-echo "Evaluate tests"
+echo "--------------------------------------------------------------------------------";
+echo "-- Evaluate tests";
+echo "--------------------------------------------------------------------------------";
 python scripts/evaluate_tests.py tests/test_more_outputs.test outputs/test_more_outputs.out -o outputs/test_more_outputs.junit.xml
+stopIfFailed $?;
 ./scripts/artifactory-deploy.sh outputs/test_more_outputs.junit.xml
-stopIfFailed()
 
-echo "Deploy master workspace"
+echo "--------------------------------------------------------------------------------";
+echo "-- Deploy master workspace";
+echo "--------------------------------------------------------------------------------";
 python scripts/workspace_deploy.py -of outputs -ow workspace.json -c tests/test.cfg -v -cn $WA_USERNAME -cp $WA_PASSWORD -cid $WA_WORKSPACE_ID_MASTER
-stopIfFailed()
+stopIfFailed $?;
 
