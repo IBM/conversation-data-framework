ce1caa3786e07b914515b7e148c685bddecf3ee5
Tereza Pytelova <tereza.pytelova@cz.ibm.com>
2018-08-13 18:51:28 +0200

    [#131] change log message

--- ci/artifactory-deploy.sh
+++ ci/artifactory-deploy.sh
@@ -9,7 +9,7 @@ echo "Artifactory: deploy ${PATH_TO_FILE}";
 for FILENAME in ${PATH_TO_FILE}; do
     echo "Artifactory: deploy ${FILENAME} to ${DIR_URL}/${FILENAME}";
     if [ -z "${ARTIFACTORY_API_KEY}" ]; then
-        echo "Artifactory: skip - API KEY is not provided";
+        echo "Artifactory: skip - API KEY is not provided for pull requests from forks";
         continue
     fi
     curl -H 'X-JFrog-Art-Api: '${ARTIFACTORY_API_KEY} -T ${FILENAME} ${DIR_URL}/${FILENAME};
