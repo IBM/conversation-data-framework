5449373669a85379146140d7d9aa8b2d093993ea
Ota Sandr <ota_sandr@cz.ibm.com>
2019-03-05 09:27:18 +0100

    [#234] unit test improvements

--- ci/unit_tests/wawCommons/getFilesAtPath_test.py
+++ ci/unit_tests/wawCommons/getFilesAtPath_test.py
@@ -1,11 +1,14 @@
 
-import os
+import os, unittest
 
 import wawCommons
- 
-def test_getFilesAtPath():
-    directory = '../tests/test_data' 
-    pathList = wawCommons.getFilesAtPath([directory])
-    testFile = os.path.abspath(os.path.join(directory, "text2codeExample-en.xml"))
-    assert testFile in pathList
+
+class TestGetFilesAtPath(unittest.TestCase):
+    
+    ''' Tests if particular file is found by getFilesAtPath function '''
+    def test_ifFileIsFound(self):
+        directory = './tests/test_data' 
+        pathList = wawCommons.getFilesAtPath([directory])
+        testFile = os.path.abspath(os.path.join(directory, "text2codeExample-en.xml"))
+        assert testFile in pathList
     
