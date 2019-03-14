c7ec884d46def6b2d63fa11e2913668462525384
tondrej <tondrej@cz.ibm.com>
2019-03-14 13:11:41 +0100

    [#17, #232] Print number of files and folder in output.zip

--- scripts/artifactory/artifactory-deploy-tests-outputs.sh
+++ scripts/artifactory/artifactory-deploy-tests-outputs.sh
@@ -7,8 +7,11 @@ cd ./ci
 for folder in `find . -name outputs`;
 do
     echo "Adding ${folder} to ${ZIP_NAME}";
-    zip -ru ${ZIP_NAME} ${folder}
+    zip -ru ${ZIP_NAME} ${folder};
 done
+
+ZIP_NUMBER=`zipinfo -1 outputs.zip | wc -l`
+echo "Total number of files and folder in ${ZIP_NAME} is ${ZIP_NUMBER}"
 
 cd ../
 ./scripts/artifactory/artifactory-deploy.sh ci/${ZIP_NAME}
