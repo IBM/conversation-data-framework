03c1dc29d49eb1f5cc6b7039a142f7b1b2842b00
Jan-Balata <Jan.Balata@ibm.com>
2019-03-25 10:32:44 +0100

    added more asserts for log messages

--- ci/unit_tests/intents_csv2json/processExample_test.py
+++ ci/unit_tests/intents_csv2json/processExample_test.py
@@ -84,6 +84,7 @@ class TestProcessExample(BaseTestCaseCapture):
             }
         result = intents_csv2json.processExample('<plant>very tall <color>green</color> tree</plant>', 'TestIntent', [])
         assert result == expectedResult
+        assert 'omitting outer tag annotation: <plant>' in self.caplog.text
 
     def test_intentWithMoreNestedEntities(self):
         ''' Test for nested entities'''
@@ -108,6 +109,7 @@ class TestProcessExample(BaseTestCaseCapture):
             }
         result = intents_csv2json.processExample('<plant><size>small</size> <color>brown</color> pine</plant>', 'TestIntent', [])
         assert result == expectedResult
+        assert 'omitting outer tag annotation: <plant>' in self.caplog.text
 
     def test_intentWithManyLevelNestedEntities(self):
         ''' Test for nested entities'''
@@ -125,19 +127,23 @@ class TestProcessExample(BaseTestCaseCapture):
             }
         result = intents_csv2json.processExample('<level2>hello <level1>world, how are you? this is <color>black</color></level1></level2>', 'TestIntent', [])
         assert result == expectedResult
+        assert 'omitting outer tag annotation: <level2>' in self.caplog.text
+        assert 'omitting outer tag annotation: <level1>' in self.caplog.text
 
     def test_intentAlreadyExisting(self):
         ''' Test for nested entities'''
         expectedResult = None
         result = intents_csv2json.processExample('<hey>nothing <test>like</test> the above</hey>', 'TestIntent', [{"text": "nothing like the above"}])
         assert result == expectedResult
+        assert 'Example used twice for the intent TestIntent, omitting: nothing like the above' in self.caplog.text
 
     def test_intentAnnontationButEmpty(self):
         ''' Test for nested entities'''
         expectedResult = None
         result = intents_csv2json.processExample('<empty></empty>', 'TestIntent', [])
         assert result == expectedResult
+        assert 'Omitting empty line for intent TestIntent after annotation tags are removed: <empty></empty>' in self.caplog.text
 
     def test_intentInvalidAnnotation(self):
         ''' Test for nested entities'''
-        self.t_exitCode(1, ['#Doing $some <tags> @ which ~ are <not> </in> </not> correct @what?', 'TestIntent', []])
+        self.t_exitCodeAndLogMessage(1, 'Invalid annotation tag for the intent TestIntent, </in>', ['#Doing $some <tags> @ which ~ are <not> </in> </not> correct @what?', 'TestIntent', []])
