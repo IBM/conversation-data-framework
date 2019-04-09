0b80ee5fadd8f1e07bbdfc7833c7a8b3d35ce58c
tondrej <tondrej@cz.ibm.com>
2019-04-09 17:50:19 +0200

    Remove unused imports

--- ci/unit_tests/evaluate_tests/main_test.py
+++ ci/unit_tests/evaluate_tests/main_test.py
@@ -13,7 +13,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import os, json, pytest
+import os
 
 import evaluate_tests
 from ...test_utils import BaseTestCaseCapture
